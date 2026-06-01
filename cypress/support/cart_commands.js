04db718018b74cfa95026cbfd024aa554b6f1a76
Ana Clara Backer <anabackercontato@gmail.com>
2026-06-01 02:14:30 -0300

    chore(support): move adicionarProdutoAoCarrinho() para arquivo mais semântico

--- cypress/support/cart_commands.js
+++ cypress/support/cart_commands.js
@@ -1,10 +1,1 @@
-Cypress.Commands.add('adicionarProdutoAoCarrinho', (index = 0) => {
-  cy.get('.features_items .product-image-wrapper')
-    .eq(index)
-    .trigger('mouseover');
 
-  cy.get('.features_items .product-image-wrapper')
-    .eq(index)
-    .contains('Add to cart')
-    .click();
-});
