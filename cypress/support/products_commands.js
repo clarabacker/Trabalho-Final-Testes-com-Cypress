f14b2b4c2380a6280f495c645f1d8922dc879346
Clara Backer <anaclara.backer@g.globo>
2026-06-01 23:45:38 -0300

    feat(products-commands): adiciona comando para validar produtos na página

--- cypress/support/products_commands.js
+++ cypress/support/products_commands.js
@@ -8,3 +8,9 @@ Cypress.Commands.add('adicionarProdutoAoCarrinho', (index = 0) => {
     .contains('Add to cart')
     .click();
 });
+
+Cypress.Commands.add('validarProdutos', () => {
+  cy.url().should('include', '/products');
+  cy.get('.features_items').should('be.visible');
+  cy.contains('All Products').should('be.visible');
+});
